5b5d6f800fdd36fcfc8a0a06161958c1a8f72424
Gabriel-Leao <gabriel.leao2507@gmail.com>
2022-09-01 18:25:57 -0300

    ajustes

--- src/components/Filters/status/index.tsx
+++ src/components/Filters/status/index.tsx
@@ -15,11 +15,11 @@ const Status = () => {
   return (
     <div className="accordion-item w-[300px]">
       <h2 className="accordion-header" id="headingOne">
-        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
           Status
         </button>
       </h2>
-      <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div id="collapseOne" className="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
         <div className="accordion-body flex flex-wrap gap-3">
           { statusList.map((item, key) => (
             <FIltersButton key={key} name="status" item={item} handleClick={handleClick} />
